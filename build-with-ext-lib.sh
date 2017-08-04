3e888f0a0fee5ac97450a2eacce6fb8ca4b6483a
MrT <sebastien.tack@sfr.fr>
2017-08-04 08:20:56 +0200

    Depot initial

--- build-with-ext-lib.sh
+++ build-with-ext-lib.sh
@@ -1,24 +1,25 @@
+# CONFIGURATION
 dir=/usr/share/arduino/hardware/arduino
 projet=test-HC05
 serie=/dev/ttyACM0
 baud=115200
 lib_dir=/home/pi/arduino/lib
 
-
+# PATCH DU FICHIER INO
 cp ./ino/$projet.ino $projet.cpp
 sed -i "1i\#include \"Arduino.h\"\n" $projet.cpp
 
-#Bien ajouter les -I des lib
+# LECTURE DE LA LISTE DES LIB
 ls $lib_dir/ > liste.txt
 
 tmp=""
-
 while read line; do 
     echo -e "$line"; 
     tmp=$tmp" -I $lib_dir/$line" 
 done < liste.txt
-
 #echo $tmp
+
+# COMPILATION DU PROJET
 
     avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=22 \
     -o $projet.o $projet.cpp \
@@ -29,19 +30,30 @@ done < liste.txt
 
 rm liste.txt
 
+# EDITION DES LIENS
+
 echo "Edition des liens"
 #linkage
 avr-gcc -Os -Wl,--gc-sections -mmcu=atmega328p -I /usr/share/arduino/libraries/SoftwareSerial -o $projet.elf $projet.o core/core.a -L core -lm
 
+
+# GENERATION DU FICHIER HEX
+
 echo "Génération du fichier HEX"
 avr-objcopy -O ihex -R .eeprom $projet.elf ./hex/$projet.hex
+
+# UPLOAD DANS L'ARDUINO
 
 echo "Upload du fichier - Arduino branché en $serie"
 avrdude -v -p m328p -P $serie -b $baud -c arduino -U flash:w:./hex/$projet.hex
 
+# NETTOYAGE
+
 rm $projet.o
 rm $projet.elf
 rm $projet.cpp
+
+# AFFICHAGE CMD MONITEUR SERIE
 
 #echo screen $serie $baud
 
--- compil-core-lib.sh
+++ compil-core-lib.sh
@@ -1,8 +1,10 @@
+# CONFIGURATION
 dir=/usr/share/arduino/hardware/arduino
 lib_dir=/home/pi/arduino/lib
 
 rm core/*.*
 
+# COMPILATION DES FICHIERS C ARDUINO
 for file in $dir/cores/arduino/*.c
 do
          filename=$(basename "$file")
@@ -15,6 +17,7 @@ do
            -o core/$filename.o $file
 done
 
+# COMPILATION DES FICHIERS C++ ARDUINO
 for file in $dir/cores/arduino/*.cpp
 do
          filename=$(basename "$file")
@@ -25,18 +28,17 @@ do
            -I$dir/cores/arduino \
            -I$dir/variants/standard \
            -o core/$filename.o $file
-
 done
 
+# COMPILATION DE SOFTWARE SERIAL
+avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=22 \
+    -I$dir/cores/arduino \
+    -I$dir/variants/standard \
+    -I /usr/share/arduino/libraries/SoftwareSerial \
+    -o core/SoftwareSerial.o /usr/share/arduino/libraries/SoftwareSerial/SoftwareSerial.cpp
 
-         avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=22 \
-           -I$dir/cores/arduino \
-           -I$dir/variants/standard \
-           -I /usr/share/arduino/libraries/SoftwareSerial \
-           -o core/SoftwareSerial.o /usr/share/arduino/libraries/SoftwareSerial/SoftwareSerial.cpp
 
-
-
+# COMPILATION DES  LIB PERSO
 echo "Compilation des librairies supplémentaires dans $lib_dir"
 ls $lib_dir/ > liste.txt
 while read line; do 
@@ -52,7 +54,7 @@ while read line; do
 done < liste.txt
 rm liste.txt
 
+# CREATION DE LA LIBRAIRIE
 echo "Création de la librairie complete"
-#assemblage
 avr-ar rcsv core/core.a core/*.o
 
